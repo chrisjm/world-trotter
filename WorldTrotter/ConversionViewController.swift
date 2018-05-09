9a15afa67aba6dfa537e64bdf9f0f808fcc34294
Chris Mears <chris.mears@gmail.com>
2018-05-09 14:59:12 -0700

    Add temperature conversion view 🌡

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Chris Mears on 5/5/18.
+//  Copyright © 2018 Wandering Leaf Studios LLC. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let decimalDigitsAndPeriodCharacterSet = CharacterSet.init(charactersIn: "0123456789.-")
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let replacementTextHasNonNumericCharacters = string.rangeOfCharacter(from: decimalDigitsAndPeriodCharacterSet.inverted)
+        
+        if (existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil) ||
+            replacementTextHasNonNumericCharacters != nil
+        {
+            return false
+        }
+        
+        return true
+    }
+}
